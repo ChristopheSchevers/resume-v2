79673b720d7c34b84b34f868b541f4b2dc17ae33
Christophe Schevers <christophe.schevers@gmail.com>
2019-06-21 17:59:12 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,28 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// Collapsibles experience list
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("mouseover", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    let content = this.nextElementSibling;
+
+    content.style.maxHeight = content.scrollHeight + "px";    
   });
 
-  coll[i].addEventListener("mouseout", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if(content.style.maxHeight) {
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-    }
+  coll[i].addEventListener("mouseout", function() {    
+    let content = this.nextElementSibling;
+    
+    content.style.maxHeight = null;    
   });
 }
+
+// Collapsible courses
+let expand = document.getElementById("courses");
+
+expand.addEventListener("mouseover", function() {
+    expand.style.maxHeight = expand.scrollHeight + "px";    
+});
+
+expand.addEventListener("mouseout", function() {
+    expand.style.maxHeight = null;    
+});
