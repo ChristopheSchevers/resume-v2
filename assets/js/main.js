f7b76f301d12511a194e6d1dc9f6081147316629
Christophe Schevers <christophe.schevers@gmail.com>
2019-06-24 16:45:24 +0200

    new version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+window.onscroll = function() {scrollFunction()};
+
 // Collapsibles experience list
 let coll = document.getElementsByClassName("collapsible");
 let i;
@@ -5,10 +7,10 @@ let i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("mouseover", function() {
     let content = this.nextElementSibling;
-
+    
     content.style.maxHeight = content.scrollHeight + "px";    
   });
-
+  
   coll[i].addEventListener("mouseout", function() {    
     let content = this.nextElementSibling;
     
@@ -26,3 +28,18 @@ expand.addEventListener("mouseover", function() {
 expand.addEventListener("mouseout", function() {
     expand.style.maxHeight = null;    
 });
+
+// Top button
+
+function scrollFunction() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("top-btn").style.display = "block";
+  } else {
+    document.getElementById("top-btn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
